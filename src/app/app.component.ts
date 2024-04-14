b72bbe08b8f3c01a98a7eaabb6814cb636041ee2
xXd4RkC0d3urXx <vazadriano289@gmail.com>
2024-04-14 18:06:28 +0200

    Premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {LightPageComponent} from "./components/light-page/light-page.component";
+import {AuthenticationComponent} from "./components/authentication/authentication.component";
+import {OidcSecurityService} from "angular-auth-oidc-client";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LightPageComponent],
+  imports: [RouterOutlet, NavbarComponent, LightPageComponent, AuthenticationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'devwebtd';
+export class AppComponent implements OnInit{
+  title = 'Network Manager';
+  private readonly oidcSecurityService = inject(OidcSecurityService);
+
+  ngOnInit() {
+    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData}) => {
+      console.log(isAuthenticated);
+      console.log(userData);
+      if (isAuthenticated) {
+        console.log("User is authenticated");
+        console.log("User data: ", userData);
+      } else {
+        console.log("User is not authenticated");
+        this.login()
+      }
+    }/* ... */);
+  }
+
+  login() {
+    this.oidcSecurityService.authorize();
+    console.log("login loop");
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff().subscribe((result) => console.log(result));
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,44 @@
-import {ApplicationConfig} from '@angular/core';
+import {ApplicationConfig, NgModule} from '@angular/core';
 import {provideRouter} from '@angular/router';
+import {provideAuth, LogLevel, AuthModule} from 'angular-auth-oidc-client';
 
 import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 
+// @ts-ignore
+/*@NgModule({
+  // ...
+  imports: [
+    // ...
+    AuthModule.forRoot({
+      config: {
+        authority: '<your authority address here>',
+        redirectUrl: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
+        clientId: '<your clientId>',
+        scope: 'openid profile email offline_access',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        logLevel: LogLevel.Debug,
+      },
+    }),
+  ],
+  // ...
+})*/
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-  provideHttpClient()]
+  provideHttpClient(),
+  provideAuth({config: {
+      authority: 'http://localhost:8080/realms/network-manager',
+      redirectUrl: "http://localhost:4200",
+      postLogoutRedirectUri: "http://localhost:4200",
+      clientId: 'angular-client',
+      scope: 'openid email profile roles web-origins',
+      responseType: 'id_token token',
+      silentRenew: true,
+      useRefreshToken: true,
+      logLevel: LogLevel.Debug,
+    }})]
 };
