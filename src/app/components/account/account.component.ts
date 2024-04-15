add1cadeb92c22f2e907af2245135cfdfb5829d7
xXd4RkC0d3urXx <vazadriano289@gmail.com>
2024-04-15 17:23:53 +0200

    Déconnexion, modifications graphiques

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AppComponent} from "../../app.component";
 
 @Component({
   selector: 'app-account',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './account.component.css'
 })
 export class AccountComponent {
+  name = JSON.parse(sessionStorage.getItem('0-angular-client') || '{}').userData?.name;
+  email = JSON.parse(sessionStorage.getItem('0-angular-client') || '{}').userData?.email;
+  appComponent: AppComponent;
 
+  constructor(appComponent : AppComponent) {
+    this.appComponent = appComponent;
+  }
+
+  logout() {
+    this.appComponent.logout();
+  }
 }
